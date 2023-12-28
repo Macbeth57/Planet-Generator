bfc85d16c6d11bbfe2c50620e1373a88b9a1c5c7
Louis <macbeth574@gmail.com>
2023-12-28 22:55:54 +0100

    Done

---
+++ PlanetGenerator/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PlanetGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(211, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 704);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 74);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 863);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PlanetGenerator/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlanetGenerator
+{
+    public partial class Form1 : Form
+    {
+        
+        private bool isButtonClicked = false;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Paint += new PaintEventHandler(panel1_Paint);
+            button1.Click += new EventHandler(button1_Click);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            isButtonClicked = true;
+
+        }
+
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            int randomSize = random.Next(101, 300);
+            int halfSize = randomSize / 2;
+            int centerX = 225 - halfSize;
+            int centerY = 225 - halfSize;
+            Brush brush = new SolidBrush(GetRandomColor());
+            g.FillEllipse(brush, centerX, centerY, randomSize, randomSize);
+
+            cratersGenerator(g);
+        }
+
+        private Color GetRandomColor()
+        {
+
+            Color randomColor = default;
+            int red = random.Next(0, 255);
+            int green = random.Next(0, 255);
+            int blue = random.Next(0, 255);
+
+            randomColor = Color.FromArgb(red, green, blue);
+            
+            return randomColor;
+
+        }
+
+
+        private void cratersGenerator(Graphics g)
+        {
+            int cratersNbr = random.Next(1, 14);
+            int craterSize = default;
+            int craterXPosition = default;
+            int craterYPosition = default;
+
+            for (int i = 0; i < cratersNbr; i++)
+            {
+                craterSize = random.Next(3, 21);
+                craterXPosition = random.Next(0, 600);
+                craterYPosition = random.Next(0, 600);
+
+                Brush smallCircleBrush = new SolidBrush(GetRandomColor());
+                g.FillEllipse(smallCircleBrush, craterXPosition, craterYPosition, craterSize, craterSize);
+            }
+        }
+        private void planetGenerator()
+        {
+
+        }
+    }
+}
